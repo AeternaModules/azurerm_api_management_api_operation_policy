687cfea1ad84b401463587146c2fec96f44ee735
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:44:54 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_api_operation_policys" {
+  description = "All api_management_api_operation_policy resources"
+  value       = azurerm_api_management_api_operation_policy.api_management_api_operation_policys
+}
+
